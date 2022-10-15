b2d7b7c8fafc809381ec2ac5d933adf813280198
Wasted-github <105445612+Wasted-github@users.noreply.github.com>
2022-10-15 23:13:43 +0700

    Add files via upload

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "Wasted#8565",
+	"id": "Wasted",
+	"patches": {
+		"RemiRetexture": "A retexture mod that replace Remilia with Marisa in Touhou 11"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/Wasted-github/touhou_patches/main/"
+	],
+	"title": "Wasted mods"
+}
