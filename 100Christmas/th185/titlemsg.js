2b99f143570ab1c7157f935a5637a32c9399f78d
Wasted-github <105445612+Wasted-github@users.noreply.github.com>
2023-01-14 23:14:29 +0700

    100Christmas patch
    baboop

---
+++ 100Christmas/th185/titlemsg.js
@@ -0,0 +1,221 @@
+{
+    "0_6": [
+        "This is my first black market. What was that stuff I needed?",
+        "The \"Bullet Money\" that comes out of bullets I hit with my magic circle?",
+        "I can summon my magic circle far away by holding it down, too."
+    ],
+    "0_0": [
+        "This is my first black market. What was that stuff I needed?",
+        "The \"Bullet Money\" that comes out of bullets I hit with my magic circle?",
+        "I can summon my magic circle far away by holding it down, too."
+    ],
+    "1_6": [
+        "A sheer cliff located in the deep regions where humans dare to tread.",
+        "Which makes it a great place for youkai to come up with evil schemes."
+    ],
+    "1_0": [
+        "Mike popped up.",
+        "It totally feels like she beckoned me in.",
+        "I guess it being a black market doesn't matter much to a maneki-neko?"
+    ],
+    "1_1": [
+        "I met Minoriko!",
+        "What's a harvest god doing at a cliff like this?",
+        "It sure doesn't seem like a good place for growing crops."
+    ],
+    "1_2": [
+        "Larva, huh?",
+        "I heard that she's been playing pranks with the Three Fairies lately.",
+        "Seems like black markets make it easy for even fairies to join in."
+    ],
+    "1_3": [
+        "Nemuno showed up.",
+        "I thought she was the kinda gal who didn't like being around people...",
+        "Oh, wait. Right. I'm checking out places where people don't normally go."
+    ],
+    "2_6": [
+        "A creepy lake that creatures can't live in.",
+        "Nothing living in it means the lake is way more clear, though."
+    ],
+    "2_0": [
+        "Cirno, eh? She does typically use this lake as her hideout.",
+        "Can she even sell stuff?",
+        "I bet I could steal some Bullet Money off of her without her even noticing..."
+    ],
+    "2_1": [
+        "It's Wakasagihime, the mermaid. A real rare character.",
+        "So the black markets are enticing enough to even attract mermaids...?",
+        "I only just noticed this, but the Ability Cards are totally waterproof, huh."
+    ],
+    "2_3": [
+        "Argh, Sekibanki popped up.",
+        "Her flying-head schtick is creepy, no matter how many times I see it.",
+        "I wonder whether her body or her head is the main part. Think I'll run some tests next time."
+    ],
+    "2_2": [
+        "I met Urumi.",
+        "Looks like she pops on down here to sell fish every now and again.",
+        "Wonder if she was doing any card-collecting on the side?"
+    ],
+    "3_6": [
+        "The Jizo shore of the Sanzu River. Just barely counts as part of the mortal realm.",
+        "It's one of Gensokyo's tourist destinations. Tends to have a lotta festival stalls."
+    ],
+    "3_0": [
+        "I met Eika!",
+        "Is she even able to leave the Jizo shore...?",
+        "Well, I do think it's a nice environment for her."
+    ],
+    "3_1": [
+        "I beat Kutaka just now.",
+        "She was pretty serious, even if she seemed like a fish out of water.",
+        "Ah, but she did open a black market. I wonder if she's more sociable than she lets on."
+    ],
+    "3_2": [
+        "Hey, I met Narumi. Wonder if she was on the clock.",
+        "I'm pretty sure I heard that Narumi's job was to stand guard and",
+        "watch so that the riverbank's stone towers don't fall over. ...The heck kinda job is that?"
+    ],
+    "3_3": [
+        "I beat Komachi.",
+        "She was hanging around unusually close to her job site.",
+        "But, well, it didn't look like she was doing her job in the first place."
+    ],
+    "4_6": [
+        "The Moriya Shrine's holding a festival day.",
+        "Nearly all the stalls are sellin' cards in black-market deals."
+    ],
+    "4_0": [
+        "I beat Sanae.",
+        "To think she'd be the type a' person to hold a black market at her own shrine...",
+        "Well, can't say I can't relate to her."
+    ],
+    "4_1": [
+        "I beat Sakuya.",
+        "You'd think she'd use that ability of hers to get up to",
+        "nastier stuff than just back-alley deals. Maybe it's not her style?"
+    ],
+    "4_2": [
+        "I beat Youmu.",
+        "I'd really rather have that sword-skills ability of hers.",
+        "Why's the lame ghost power the one that gets a card?"
+    ],
+    "4_3": [
+        "I beat Reimu. And after she went an' nagged me",
+        "about leaving the black markets alone, too...!",
+        "I'm gonna investigate who's settin' these things up, so you wait right there. On the ground."
+    ],
+    "4_4": [
+        "I never woulda thought that Nitori was Chimata's accomplice.",
+        "So all I gotta do to fight the black market mastermind is to",
+        "equip the \"Capitalist's Dilemma\" card and go to the Rainbow Dragon Cave, yeah?"
+    ],
+    "5_6": [
+        "The Youkai Mountain is a lot livelier than usual.",
+        "There's youkai huntin' for black markets here too."
+    ],
+    "5,0": [
+        "I beat Tsukasa.",
+        "Does she ever cut her bangs? They must be annoyin' as hell to deal with.",
+        "Or maybe... that's what's... uh, cool...?"
+    ],
+    "5_1": [
+        "I beat Megumu.",
+        "Seems like the big, important Great Tengu doesn't even know how to use a tripod.",
+        "It's not a blunt implement. It's a chair. Better teach her that next time."
+    ],
+    "5_2": [
+        "I met Clownpiece.",
+        "What's she doin' on Youkai Mountain? Oh right, I guess she",
+        "musta gotten lost on her way to the Moriya Shrine."
+    ],
+    "5_3": [
+        "I met Tenshi.",
+        "Wonder how many keystones she even has.",
+        "With how many she's got, there oughta be no more earthquakes."
+    ],
+    "6_6": [
+        "A secret dream world passage that also linked to the Youkai Mountain.",
+        "It seems short, but the fairies are pretty strict in guarding, too."
+    ],
+    "6_0": [
+        "A dream dweller of Suika was boozin' it up in there.",
+        "Hey, woah, it looks like she was hostin' an entire party in the passage.",
+        "Did she fill the place with fog to make things harder...? Talk about brash."
+    ],
+    "6_1": [
+        "A dream dweller of Mamizou was in there gettin' sloshed.",
+        "Havin' a drink in that passage? That's dream dweller for ya.",
+        "Better exterminate her, or she'll disguises as us and cause more troubles."
+    ],
+    "6_2": [
+        "The heck is a dream dweller of Saki doin' in there?",
+        "Oh, I see, she's still lookin' for strong people to join her clan.",
+        "Hang on, does Dream World even have Hell or Animal Realms to begin with?"
+    ],
+    "6_3": [
+        "Seriously, the dream dweller of Momoyo creeps me the heck out.",
+        "Was she the one that invited all the strong guys here?",
+        "Guess even bugs are all the rage with dream dwellers, huh?"
+    ],
+    "6_4": [
+        "So the black markets were Doremy's doing all along.",
+        "I mean like it's a special occasion, after all.",
+        "Oh well, guess I'll enjoy myself at these black markets just a li'l while longer."
+    ],
+    "7_6": [
+        "There's a rainbow 'round the moon.",
+        "This is where a legitimate market would be held... ordinarily."
+    ],
+    "7_0": [
+        "Obviously, Chimata knew all about those black markets, too.",
+        "Think she told me to go see an accomplice of hers at the Moriya Shrine.",
+        "Guess that's where I'm headed next, \"Blank Card\" in hand..."
+    ],
+    "8_6": [
+        "The god of marketplaces said she's gonna formally oversee the black markets.",
+        "But then wouldn't they just be regular markets?",
+        "There's so many youkai showin' up here, it's gonna get crazy in a sec..."
+    ],
+    "8_0": [
+        "The god of marketplaces said she's gonna formally oversee the black markets.",
+        "But then wouldn't they just be regular markets?",
+        "There's so many youkai showin' up here, it's gonna get crazy in a sec..."
+    ],
+    "-1_0": [
+        "???",
+        "Mike Goutokuji",
+        "Mike Goutokuji",
+        "Minoriko Aki",
+        "Eternity Larva",
+        "Nemuno Sakata",
+        "Cirno",
+        "Wakasagihime",
+        "Sekibanki",
+        "Urumi Ushizaki",
+        "Eika Ebisu",
+        "Kutaka Niwatari",
+        "Narumi Yatadera",
+        "Komachi Onozuka",
+        "Sanae Kochiya",
+        "Sakuya Izayoi",
+        "Youmu Konpaku",
+        "Reimu Hakurei",
+        "Tsukasa Kudamaki",
+        "Megumu Iizunamaru",
+        "Clownpiece",
+        "Tenshi Hinanawi",
+        "Suika Ibuki",
+        "Mamizou Futatsuiwa",
+        "Saki Kurokoma",
+        "Momoyo Himemushi",
+        "Chimata Tenkyuu",
+        "Nitori Kawashiro",
+        "Takane Yamashiro"
+    ],
+    "5_0": [
+        "I beat Tsukasa.",
+        "Does she ever cut her bangs? They must be annoyin' as hell to deal with.",
+        "Or maybe... that's what's... uh, cool...?"
+    ]
+}
