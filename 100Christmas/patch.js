6dcc3b3fbdbf5c2f723d94a932a9489b1657d551
Wasted-github <105445612+Wasted-github@users.noreply.github.com>
2023-01-14 23:23:54 +0700

    100Christmas patch.js addition
    I tried my best idk if it's even correct lmao

---
+++ 100Christmas/patch.js
@@ -0,0 +1,13 @@
+{
+	"dependencies":[
+		"nmglc/base_tsa",
+		"nmglc/script_latin",
+		"nmglc/western_name_order"
+	],
+	"id": "Wasted",
+	"servers": [
+		"https://mirrors.thpatch.net/Wasted/100Christmas/"
+	],
+	"title": "(th18) 100Christmas mod",
+	"update": true
+}
