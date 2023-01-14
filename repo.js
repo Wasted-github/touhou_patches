e02a1ea02fa6e78e28cf45ff08190cfa4f012581
Wasted-github <105445612+Wasted-github@users.noreply.github.com>
2023-01-14 23:20:26 +0700

    100Christmas patch addition
    baboo

--- repo.js
+++ repo.js
@@ -3,6 +3,7 @@
 	"id": "Wasted",
 	"patches": {
 		"RemiRetexture": "(th11) A retexture mod that replace Marisa with Remilia"
+		"100Christmas": "(th185) A small Christmas mod featuring a replacement of Takane with Doremy with 3 new spellcards, stage background replacement and a bit enemy waves modification"
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/Wasted/"
