de9aa3645cf65e6460171c738fc61ac434ab5bf7
Wasted-github <105445612+Wasted-github@users.noreply.github.com>
2023-01-15 18:25:00 +0700

    add a comma
    baboop

--- repo.js
+++ repo.js
@@ -2,7 +2,7 @@
 	"contact": "Wasted#8565",
 	"id": "Wasted",
 	"patches": {
-		"RemiRetexture": "(th11) A retexture mod that replace Marisa with Remilia"
+		"RemiRetexture": "(th11) A retexture mod that replace Marisa with Remilia",
 		"100Christmas": "(th185) A small Christmas mod featuring a replacement of Takane with Doremy with 3 new spellcards, stage background replacement and a bit enemy waves modification"
 	},
 	"servers": [
